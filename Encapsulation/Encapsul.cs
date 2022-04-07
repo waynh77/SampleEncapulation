c690a529e6dd34ea649a89b38600cb55e013fc4e
wahyu nurhidayat <waynh77@gmail.com>
2022-04-07 17:58:23 +0700

    upload

---
+++ Encapsulation/Encapsul.cs
@@ -0,0 +1,19 @@
+﻿
+namespace Encapsulation
+{
+    internal class Encapsul
+    {
+        public string? nama;
+        public string? alamat;
+        public int usia;
+        public void SetUmur(int umur)
+        {
+            usia = umur;
+        }
+
+        public int GetUmur()
+        {
+            return usia;
+        }
+    }
+}
---
+++ Encapsulation/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using Encapsulation;
+
+Encapsul data = new();
+
+data.nama = "Wahyu";
+data.alamat = "Jakarta";
+data.SetUmur(17);
+Console.WriteLine("Nama = "+data.nama);
+Console.WriteLine("Alamat = "+data.alamat);
+Console.WriteLine("Umur = "+data.GetUmur());
+Console.ReadKey();
